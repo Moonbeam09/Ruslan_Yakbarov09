07a604fac3137ac9c80ad9d1192b8baab73db824
itisioslab <itisioslab@Mac-mini-Administrator.local>
2014-08-19 11:18:57 +0400

    Initial Commit

---
+++ RYIOSLAB/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  RYIOSLAB
+//
+//  Created by itisioslab on 19.08.14.
+//  Copyright (c) 2014 itisioslab. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
